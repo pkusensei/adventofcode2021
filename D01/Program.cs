c3e5eee0af8c4780cfdbd7407f90e61d08672c27
pkusensei <ZW_08@outlook.com>
2021-12-01 11:57:17 -0500

    day 01

---
+++ D01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+var test = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263".Split('\n').Select(x =>
+        Int32.Parse(x)
+    ).ToList();
+
+static int p1(IList<int> nums)
+{
+    return nums.Skip(1).Where((value, idx) => value > nums[idx]).Count();
+}
+
+Debug.Assert(p1(test) == 7);
+
+var input = File.ReadLines("input.txt").Select(x =>
+        Int32.Parse(x)
+    ).ToList();
+
+Debug.Assert(p1(input) == 1665);
+
+static int p2(IList<int> nums)
+{
+    return nums.TakeWhile((value, idx) => idx < nums.Count - 3)
+    .Where((value, idx) => value + nums[idx + 1] + nums[idx + 2] < nums[idx + 1] + nums[idx + 2] + nums[idx + 3]).Count();
+}
+
+Debug.Assert(p2(test) == 5);
+Debug.Assert(p2(input) == 1702);
